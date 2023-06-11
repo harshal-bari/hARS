bf1229fa84953770854b50d7c357afd350e2dabb
harshal-bari <129711638+harshal-bari@users.noreply.github.com>
2023-06-11 20:35:24 +0530

    Add files via upload

---
+++ pr2.cpp
@@ -0,0 +1,13 @@
+#include<iodtream.h>
+#include<conio.h>
+int main ()
+{
+    int a,b;
+    cout<<"enter the value a:";
+    cin>>a;
+    cout<<"enter the value b:";
+    cin>>b;
+    cout<<"enter value is a:"<<a<<endl;
+    cout<<"enter value is b:"<<b<<endl;
+    return 0;
+}
---
+++ pr3 - Copy.cpp
@@ -0,0 +1,6 @@
+#include<iostream.h>
+{
+    int main()
+    cout<<"hello world"l;
+ return 0;    
+}
